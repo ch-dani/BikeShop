78e1376b6ebf7fa8bac556f324c6bf2e4b89c69d
madnan <adnanchauhdhary@gmail.com>
2020-04-30 05:36:44 -0700

    i added all routes and

--- routes/user.js
+++ routes/user.js
@@ -12,6 +12,14 @@ const mongoose=require('mongoose');
 const usermodel=require('../models/usersmodel');
 app.use(bodyParser.json());
 
+const session=require('express-session');
+app.use(session({secret:'ssshhhhh,a secret',saveUninitialized: true,resave: true}));
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/dbName',{useNewUrlParser:true,useUnifiedTopology:true});
+
 router.get('/',(req,res)=>{
     res.render("index");
     
@@ -21,21 +29,72 @@ router.get('/',(req,res)=>{
  router.get('/login',(req,res)=>{
     res.render("login");
  })
- router.get('/products',(req,res)=>{
-    res.send("PROducts are not available yet")
- })
+
+
+ 
+
+
  router.get('/signup',(req,res)=>{
     res.render("signup");
  })
+
+ router.get('/products',(req,res)=>{
+    const prod=[
+      {
+         title:'productsA',
+         desp:'the best prducts2',
+         price:'454858',
+         addre:'../Assets/Images/slide3.3b069.png'
+      },
+      {
+         title:'productsA',
+         desp:'the best prducts3',
+         price:'454858',
+         addre:'../Assets/Images/slide3.3b069.png'
+      },
+      {
+         title:'productsA',
+         desp:'the best prducts4',
+         price:'454858',
+         addre:'../Assets/Images/slide3.3b069.png'
+      }
+
+    ]
+   res.render("products",{prod:prod});
+
+})
+
+
  router.get('/user-profile',(req,res)=>{
     res.send("User profile NOT AVAILABLE YES");
  })
+ 
 
  router.post('/login',(req,res)=>{
    res.send(req.body.email+'   and   '+req.body.pwd);
-   console.log(req.body.email);
-   console.log(req.body.pwd);
+ 
+   mongoose.connect('mongodb://localhost:27017/dbName',{useNewUrlParser:true,useUnifiedTopology:true});
+  
+   var db = mongoose.connection;
+  
+
+   var TheRslt=usermodel.find({ 'email': req.body.email,'pwd':req.body.pwd});
+   // TheRslt.select('name address');
+   TheRslt.limit(3);
+   
+  
+   
+   TheRslt.exec((err,athlete)=>{
+      if(err) throw err;
+      console.log(athlete[0].name);
+   })
+
+   
+  
 })
+  
+
+
 
 
 
@@ -45,10 +104,15 @@ router.post('/Signup',(req,res)=>{
    let usera=new usermodel(req.body);
    
    console.log(usera);
-   mongoose.connect('mongodb://localhost:27017/dbName',{useNewUrlParser:true,useUnifiedTopology:true});
-   usera.save().then().catch();
+   
+   usera.save().then((req,res)=>{
+      console.log("its ok");
+      res.send("hellow world!");
+   }).catch((req,res)=>{
+      console.log("NOT OK")
+   });
    res.redirect('/');
-  
+   
 
 
   
